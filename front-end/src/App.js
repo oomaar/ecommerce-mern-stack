74838ca44f79ec293a2b6456140b7c2c213321d0
Omar <omaar_5@hotmail.com>
2021-03-25 10:25:36 +0200

    setup front-end routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,18 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
+import { CartScreen, HomeScreen, ProductScreen } from "./Screens";
 
 function App() {
   return (
-    <div>
-      hello MERN
-    </div>
+    <Router>
+      <main>
+        <Switch>
+          <Route exact path="/" component={HomeScreen} />
+          <Route exact path="/product/:id" component={ProductScreen} />
+          <Route exact path="/cart" component={CartScreen} />
+        </Switch>
+      </main>
+    </Router>
   );
 }
 
