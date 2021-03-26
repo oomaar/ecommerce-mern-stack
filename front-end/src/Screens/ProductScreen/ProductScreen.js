6c4538a8d6e3c55bc35f1434d37442ef299208e4
Omar <omaar_5@hotmail.com>
2021-03-26 21:58:46 +0200

    complete dynamic product screen

--- front-end/src/Screens/ProductScreen/ProductScreen.js
+++ front-end/src/Screens/ProductScreen/ProductScreen.js
@@ -1,43 +1,77 @@
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getProductDetails } from "../../redux/actions/productActions";
+import { addToCart } from "../../redux/actions/cartActions";
 import "./ProductScreen.css";
 
-const ProductScreen = () => {
+const ProductScreen = ({ match, history }) => {
+    const [qty, setQty] = useState(1);
+    const dispatch = useDispatch();
+    const productDetails = useSelector(state => state.getProductDetails);
+    const {
+        loading,
+        error,
+        product
+    } = productDetails;
+
+    useEffect(() => {
+        if (product && match.params.id !== product._id) {
+            dispatch(getProductDetails(match.params.id));
+        }
+    }, [dispatch, product, match]);
+
+    const handleAddToCart = () => {
+        dispatch(addToCart(product._id, qty));
+        history.push("/cart");
+    }
+
     return (
         <div className="productscreen">
-            <div className="productscreen__left">
-                <div className="left__image">
-                    <img src="https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80" alt="ai alt" />
-                </div>
-                <div className="left__info">
-                    <p className="left__name">Product 1</p>
-                    <p className="left__price">$499.99</p>
-                    <p className="left__description">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Quos id fuga neque fugit blanditiis adipisci?
-                    </p>
-                </div>
-            </div>
-            <div className="productscreen__right">
-                <div className="right__info">
-                    <p>
-                        Price: <span>$499.99</span>
-                    </p>
-                    <p>
-                        Status: <span>In Stock</span>
-                    </p>
-                    <p>
-                        QTY:
-                        <select>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                        </select>
-                    </p>
-                    <p>
-                        <button type="button">Add To Cart</button>
-                    </p>
-                </div>
-            </div>
+            {loading
+                ? <h2>Spinner...</h2>
+                : error
+                    ? <h2>{error}</h2>
+                    : (
+                        <>
+                            <div className="productscreen__left">
+                                <div className="left__image">
+                                    <img src={product.imageUrl} alt={product.name} />
+                                </div>
+                                <div className="left__info">
+                                    <p className="left__name">{product.name}</p>
+                                    <p className="left__price">${product.price}</p>
+                                    <p className="left__description">{product.description}</p>
+                                </div>
+                            </div>
+                            <div className="productscreen__right">
+                                <div className="right__info">
+                                    <p>
+                                        Price: <span>${product.price}</span>
+                                    </p>
+                                    <p>
+                                        Status: <span>{
+                                            product.countInStock > 0 ? "In Stock" : "Out Of Stock"
+                                        }</span>
+                                    </p>
+                                    <p>
+                                        QTY:
+                                        <select
+                                            value={qty}
+                                            onChange={e => setQty(e.target.value)}
+                                        >
+                                            {[...Array(product.countInStock).keys()]
+                                                .map(opt => (
+                                                    <option key={opt + 5} value={opt + 1}>{opt + 1}</option>
+                                                ))}
+                                        </select>
+                                    </p>
+                                    <p>
+                                        <button onClick={handleAddToCart} type="button">Add To Cart</button>
+                                    </p>
+                                </div>
+                            </div>
+                        </>
+                    )}
         </div>
     );
 };
