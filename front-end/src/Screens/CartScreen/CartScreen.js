0c697c830503129f6a4e79b0f6a94b67a87ea980
Omar <omaar_5@hotmail.com>
2021-03-26 19:17:40 +0200

    complete cartScreen

--- front-end/src/Screens/CartScreen/CartScreen.js
+++ front-end/src/Screens/CartScreen/CartScreen.js
@@ -1,10 +1,28 @@
+import { CartItem } from "../../Components";
 import "./CartScreen.css";
 
 const CartScreen = () => {
     return (
-        <h1>
-            Cart Screen
-        </h1>
+        <div className="cartscreen">
+            <div className="cartscreen__left">
+                <h2>Shopping Cart</h2>
+                <CartItem />
+                <CartItem />
+                <CartItem />
+                <CartItem />
+                <CartItem />
+                <CartItem />
+            </div>
+            <div className="cartscreen__right">
+                <div className="cartscreen__info">
+                    <p>Subtotal (0) items</p>
+                    <p>$499.99</p>
+                </div>
+                <div className="cartscreen__button">
+                    <button>Proceed To Checkout</button>
+                </div>
+            </div>
+        </div>
     );
 };
 
