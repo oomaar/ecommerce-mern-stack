b447eb0f327663e13488fedbcf98158fd17dcd8e
Omar <omaar_5@hotmail.com>
2021-03-27 17:24:04 +0200

    refactor

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,22 +4,22 @@ import { CartScreen, HomeScreen, ProductScreen } from "./Screens";
 import { Backdrop, Navbar, Sidebar } from "./Components";
 import './App.css';
 import { GlobalStyles } from "./GlobalStyles";
+import { selectDarkMode } from "./redux/darkModeSlice";
+import { useSelector } from "react-redux";
 
 function App() {
   const [toggleShow, setToggleShow] = useState(false);
-  const [darkMode, setDarkMode] = useState(false);
+  const dark = useSelector(selectDarkMode);
 
   return (
     <Router>
-      <GlobalStyles darkMode={darkMode} />
-      <Navbar darkMode={darkMode} setDarkMode={setDarkMode} click={() => setToggleShow(true)} />
-      <Sidebar darkMode={darkMode} click={() => setToggleShow(false)} toggleShow={toggleShow} />
-      <Backdrop darkMode={darkMode} click={() => setToggleShow(false)} toggleShow={toggleShow} />
+      <GlobalStyles dark={dark} />
+      <Navbar click={() => setToggleShow(true)} />
+      <Sidebar click={() => setToggleShow(false)} toggleShow={toggleShow} />
+      <Backdrop click={() => setToggleShow(false)} toggleShow={toggleShow} />
       <main>
         <Switch>
-          <Route exact path="/">
-            <HomeScreen darkMode={darkMode} />
-          </Route>
+          <Route exact path="/" component={HomeScreen} />
           <Route exact path="/product/:id" component={ProductScreen} />
           <Route exact path="/cart" component={CartScreen} />
         </Switch>
--- front-end/src/GlobalStyles.js
+++ front-end/src/GlobalStyles.js
@@ -10,7 +10,7 @@ export const GlobalStyles = createGlobalStyle`
   body {
     margin: 0;
     font-family: Arial, Helvetica, sans-serif;
-    background: ${({darkMode}) => darkMode ? '#f4f4f4' : '#171717'};
+    background: ${({dark}) => dark ? '#171717' : '#f4f4f4'};
     transition: background 0.5s ease-in;
   }
   
