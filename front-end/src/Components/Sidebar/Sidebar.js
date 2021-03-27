0d759df5de5d45ed748c35938fedf22ddeda4dd6
Omar <omaar_5@hotmail.com>
2021-03-27 13:23:21 +0200

    implement cart count in Sidebar

--- front-end/src/Components/Sidebar/Sidebar.js
+++ front-end/src/Components/Sidebar/Sidebar.js
@@ -1,12 +1,20 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import "./Sidebar.css";
 
 const Sidebar = ({ toggleShow, click }) => {
     const sideBarClass = ['sidebar'];
+    const cart = useSelector(state => state.cart);
+    const { cartItems } = cart;
 
     if (toggleShow) {
         sideBarClass.push('toggleShow');
     };
+
+    const getCartCount = () => {
+        return cartItems.reduce((qty, item) => qty + Number(item.qty), 0);
+    };
+
 
     return (
         <div className={sideBarClass.join(" ")}>
@@ -15,7 +23,7 @@ const Sidebar = ({ toggleShow, click }) => {
                     <Link to="/cart">
                         <i className="fas fa-shopping-cart"></i>
                         <span>
-                            Cart <span className="sidebar__cartBadge">0</span>
+                            Cart <span className="sidebar__cartBadge">{getCartCount()}</span>
                         </span>
                     </Link>
                 </li>
