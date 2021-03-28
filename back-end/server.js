8676b73e96ac5d2b0e410133df90a43dca4521f8
Omar <omaar_5@hotmail.com>
2021-03-28 13:44:57 +0200

    deploy

--- back-end/server.js
+++ back-end/server.js
@@ -11,8 +11,8 @@ const app = express();
 app.use(express.json());
 app.use('/api/products', productRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/front-end/build')));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '../front-end/build')));
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'front-end', 'build', 'index.html'));
     });
