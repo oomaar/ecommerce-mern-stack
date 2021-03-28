09bdb9c83537a8002eb11733b0b74d07856a8164
Omar <omaar_5@hotmail.com>
2021-03-28 12:37:39 +0200

    deploy commit

--- back-end/server.js
+++ back-end/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require('express');
 const connectDB = require('./config/db');
 const productRoutes = require('./routes/productRoutes');
+const path = require('path');
 
 connectDB();
 
@@ -10,6 +11,17 @@ const app = express();
 app.use(express.json());
 app.use('/api/products', productRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/front-end/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'front-end', 'build', 'index.html'));
+    });
+} else {
+    app.get('/', (req, res) => {
+        res.send('Api running');
+    });
+}
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server is up and running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server is up and running on port ${PORT}`));
